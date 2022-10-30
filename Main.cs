90a1379d8dc447c8da697e4bd2a18749a41349c3
Andrew White <andrew@finbuckle.com>
2022-10-29 20:40:41 -0600

    Initial

---
+++ Main.cs
@@ -0,0 +1,6 @@
+﻿using MacTest;
+
+// This is the main entry point of the application.
+NSApplication.Init ();
+NSApplication.Main (args);
+
---
+++ ViewController.cs
@@ -0,0 +1,26 @@
+﻿using ObjCRuntime;
+
+namespace MacTest;
+
+public partial class ViewController : NSViewController {
+	protected ViewController (NativeHandle handle) : base (handle)
+	{
+	}
+
+	public override void ViewDidLoad ()
+	{
+		base.ViewDidLoad ();
+
+		// Do any additional setup after loading the view.
+	}
+
+	public override NSObject RepresentedObject {
+		get => base.RepresentedObject;
+		set {
+			base.RepresentedObject = value;
+
+			// Update the view, if already loaded.
+		}
+	}
+}
+
